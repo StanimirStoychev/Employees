c599410252902efb8dcf4a06d3bd7b4777a43253
Stanimir <stmbksa@gmail.com>
2023-05-29 15:15:14 +0300

    commit project

---
+++ src/main/java/com/example/employees/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.employees.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String department;
+}
